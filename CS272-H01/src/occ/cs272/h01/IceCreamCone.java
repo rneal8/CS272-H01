4990a4547482fae92ef89b2059983b6bf13ed60a
rneal8 <rneal8@OVDIMC10-307V.coast.cccd.adx>
2019-09-09 13:13:53 -0700

    IceCream

--- CS272-H01/src/occ/cs272/h01/IceCreamCone.java
+++ CS272-H01/src/occ/cs272/h01/IceCreamCone.java
@@ -5,19 +5,32 @@
 package occ.cs272.h01;
 
 /**
- * @author <your OCC login name such as sgilbert>
- * @version <the date>
+ * @author rneal8
+ * @version 9/9/19
  * 
  */
 public class IceCreamCone
 {
-    // TODO: You must fill these in correctly to get credit
-    public static final String STUDENT = "Put your login ID here";
+    public static final String STUDENT = "rneal8";
     public static final String ASSIGNMENT = "H01-B";
 
-    // TODO: Complete this class
+    private double height;
+    private double radius;
     
+    public IceCreamCone(double h, double r)
+    {
+        height = h;
+        radius = r;
+    }
     
+    public double getSurfaceArea()
+    {
+        return (Math.PI * radius * (radius + Math.sqrt( Math.pow(radius, 2) + Math.pow(height, 2)))) - (Math.PI *  Math.pow(radius, 2));
+    }
     
+    public double getVolume()
+    {
+        return (Math.PI * Math.pow(radius, 2) * height) / 3.0; 
+    }
     
 }
