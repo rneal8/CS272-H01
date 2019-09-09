a86c3228be7e39fce28a1837adf93d294f5249fb
rneal8 <rneal8@OVDIMC10-307V.coast.cccd.adx>
2019-09-09 13:36:03 -0700

    Grade

--- CS272-H01/src/occ/cs272/h01/Grade.java
+++ CS272-H01/src/occ/cs272/h01/Grade.java
@@ -7,14 +7,14 @@ package occ.cs272.h01;
 import java.util.Scanner;
 
 /**
- * @author <your OCC login name such as sgilbert>
- * @version <the date>
+ * @author rneal8
+ * @version 9/9/19
  *
  */
 public class Grade
 {
     // TODO: You must fill these in correctly to get credit
-    public static final String STUDENT = "Put your OCC login name here";
+    public static final String STUDENT = "rneal8";
     public static final String ASSIGNMENT = "H01-C";
     
     private String grade;
@@ -34,10 +34,90 @@ public class Grade
      */
     public double getNumericGrade() 
     {
-        //TODO Complete this method
+        double result = -1.0;
         
-        
-        return 10; 
+        switch(grade) {
+            case "F":
+                result = 0.0;
+                break;
+            case "D-":
+                result = 0.7;
+                break;
+            case "D":
+                result = 1.0;
+                break;
+            case "D+":
+                result = 1.3;
+                break;
+            case "C-":
+                result = 1.7;
+                break;
+            case "C":
+                result = 2.0;
+                break;
+            case "C+":
+                result = 2.3;
+                break;
+            case "B-":
+                result = 2.7;
+                break;
+            case "B":
+                result = 3.0;
+                break;
+            case "B+":
+                result = 3.3;
+                break;
+            case "A-":
+                result = 3.7;
+                break;
+            case "A":
+                result = 4.0;
+                break;
+            case "A+":
+                result = 4.0;
+                break;
+            case "f":
+                result = 0.0;
+                break;
+            case "d-":
+                result = 0.7;
+                break;
+            case "d":
+                result = 1.0;
+                break;
+            case "d+":
+                result = 1.3;
+                break;
+            case "c-":
+                result = 1.7;
+                break;
+            case "c":
+                result = 2.0;
+                break;
+            case "c+":
+                result = 2.3;
+                break;
+            case "b-":
+                result = 2.7;
+                break;
+            case "b":
+                result = 3.0;
+                break;
+            case "b+":
+                result = 3.3;
+                break;
+            case "a-":
+                result = 3.7;
+                break;
+            case "a":
+                result = 4.0;
+                break;
+            case "a+":
+                result = 4.0;
+                break;
+        }
+                
+        return result; 
     }
     
     /**
